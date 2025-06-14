e5080cd33f8fa61b63516c9f7c50fde21267d297
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-14 11:15:43 +0200

    Snack 5

--- script.js
+++ script.js
@@ -108,9 +108,6 @@ const ageAvg = agesSum / ages.length;
 console.log(ageAvg);
 
 
-
-
-
 // Snack 5 (Bonus) - Raccogli i libri
 // Nota: a differenza di quanto visto finora negli esempi, per accedere all'API utilizzare l'url base:
 // http://localhost:3333
@@ -124,8 +121,35 @@ console.log(ageAvg);
 // Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
 // Testala con l’array [2, 13, 7, 21, 19] .
 
+async function fetchJson(url) {
+    const response = await fetch(url);
+    const obj = await response.json();
+    return obj
+}
 
+async function getBooks() {
+    try {
+        const ids = [2, 13, 7, 21, 19];
+        const promises = ids.map(id => {
+            return fetchJson(`http://localhost:3333/books/${id}`)
+        })
 
+        const books = await Promise.all(promises);
+
+        return books
+    } catch (error) {
+        throw new Error("Errore nel recupero dei dati: " + error.message)
+    }
+}
+
+(async () => {
+    try {
+        const data = await getBooks();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+})()
 
 
 // Snack 6 (Bonus) - Ordina i libri
